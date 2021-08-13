d70e44ee0e8f9ce691c722779a0c1c57157690d4
fuckingallexams <88894107+fuckingallexams@users.noreply.github.com>
2021-08-13 15:20:00 +0000

    Create bot_group_config.py

---
+++ bot_group_config.py
@@ -0,0 +1,7 @@
+# coding=utf-8
+# o means object, you can learn abbreviation nomenclature to speed up your rate
+config = c = {
+"bot1":{"api_token":"{your api_token}","o1":""},
+"bot2":{"api_token":"{your api_token}","o1":""},
+"bot3":{"api_token":"{your api_token}","o1":""}
+}
